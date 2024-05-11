bc4bdb10a42c14afc3da690d561728ffb437f360
jhu vitoria <jhuliavitoria226@gmail.com>
2024-05-11 12:20:16 -0300

    programa concluído!

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Xml;
+
+int glicemia;
+string diagnostico;
+ConsoleColor corExibicao;
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("Glicemia (em mg/dL)...: ");
+glicemia = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+if (glicemia > 126)
+{
+    diagnostico = "Diabetes Mellitus na gravidez";
+    corExibicao = ConsoleColor.Red;
+}
+else
+{
+    if (glicemia >= 92)
+    {
+        diagnostico = "Compatível com Diabetes Gestacional";
+        corExibicao = ConsoleColor.Green;
+    }
+    else
+    {
+        diagnostico = "Reavaliar entre 24 e 28 semanas com TTOG 75g de glicose";
+        corExibicao = ConsoleColor.Yellow;
+    }
+}
+
+Console.ForegroundColor = corExibicao;
+Console.WriteLine();
+Console.WriteLine(diagnostico);
+Console.ResetColor();
